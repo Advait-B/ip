6f38de76a27de9f90e093daa80edcac617afe0b1
Advait Bagri <advait.bagri@gmail.com>
2026-01-24 19:28:15 +0800

    Level-0: Rename bot to Mango, change greeting

---
+++ src/main/java/Mango.java
@@ -0,0 +1,8 @@
+public class Mango {
+    public static void main(String[] args) {
+        System.out.println("hey there, i'm mango!");
+        System.out.println("how can i help?!");
+        System.out.println();
+        System.out.println("bye! hope to see you again");
+    }
+}
