6463b08e43760f18219afc3cffb3e45e4514ffa2
Advait Bagri <advait.bagri@gmail.com>
2026-01-25 13:04:52 +0800

    Level-1: Echo

--- src/main/java/Mango.java
+++ src/main/java/Mango.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Mango {
     public static void main(String[] args) {
-        System.out.println("hey there, i'm mango!");
-        System.out.println("how can i help?!");
-        System.out.println();
+        Scanner in = new Scanner(System.in);
+        System.out.println("hey there, i'm mango!" + System.lineSeparator() + "how can i help?");
+        String input = in.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = in.nextLine();
+        }
         System.out.println("bye! hope to see you again");
     }
 }
