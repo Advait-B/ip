1d2805d3a7eb148d8b2950ca55ac91e4427a14f7
Advait Bagri <advait.bagri@gmail.com>
2026-02-01 15:32:12 +0800

    Code formatting and error catching

--- src/main/java/Mango.java
+++ src/main/java/Mango.java
@@ -2,35 +2,57 @@ import java.util.Scanner;
 
 public class Mango {
 
+    private static final int MAX_TASKS = 100;
+
     public static void printList(Task[] tasks, int taskCount) {
         int undoneCount = 0;
         for (int i = 0; i < taskCount; i++) {
-            if (!tasks[i].isDone) undoneCount++;
+            if (!tasks[i].isDone()) {
+                undoneCount++;
+            }
         }
-        if (taskCount - undoneCount != 0) System.out.println("hooray! you have done " + (taskCount - undoneCount) + " task(s) :)");
-        System.out.println("BUT oh dear, you have " + undoneCount + " task(s) you MUST complete!");
+        if (taskCount - undoneCount != 0) {
+            System.out.println("hooray! you have completed " + (taskCount - undoneCount) + " task(s) :)");
+        }
+        if (undoneCount != 0) {
+            System.out.println("oh dear, you have " + undoneCount + " task(s) you MUST complete!");
+        }
         for (int i = 1; i <= taskCount; i++) {
             System.out.println(i + ". " + tasks[i - 1]);
         }
     }
+
     public static void markAsDone(Task[] tasks, int taskCount, int index) {
         if (index < 0 || index >= taskCount) {
-            System.out.println("invalid task number... don't scam me :(");
+            System.out.println("invalid task number :(");
             return;
         }
-        tasks[index].markAsDone();
+        if (tasks[index].isDone()) {
+            System.out.println("this task is already marked as done!");
+        } else {
+            tasks[index].markAsDone();
+            System.out.println("hooray! you've completed this task. i've marked it as done: ");
+            System.out.println(tasks[index]);
+        }
     }
+
     public static void unmark(Task[] tasks, int taskCount, int index) {
         if (index < 0 || index >= taskCount) {
-            System.out.println("invalid task number... don't scam me :(");
+            System.out.println("invalid task number :(");
             return;
         }
-        tasks[index].unmark();
+        if (!tasks[index].isDone()) {
+            System.out.println("this task is already unmarked!");
+        } else {
+            tasks[index].unmark();
+            System.out.println("oh dear, it seems you have NOT completed this task. i've unmarked it: ");
+            System.out.println(tasks[index]);
+        }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Task[] tasks = new Task[100];
+        Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
 
         System.out.println("hey there, i'm mango!" + System.lineSeparator() + "how can i help?");
@@ -40,18 +62,32 @@ public class Mango {
             if (input.equals("list")) {
                 printList(tasks, taskCount);
             } else if (input.startsWith("mark ")) {
-                input = input.substring(input.indexOf(" ") + 1);
-                int index = Integer.parseInt(input) - 1;
-                markAsDone(tasks, taskCount, index);
-            } else if (input.startsWith("unmark ")) { // FIXED
-                input = input.substring(input.indexOf(" ") + 1);
-                int index = Integer.parseInt(input) - 1;
-                unmark(tasks, taskCount, index);
+                try {
+                    input = input.substring(input.indexOf(" ") + 1).trim();
+                    int index = Integer.parseInt(input) - 1;
+                    markAsDone(tasks, taskCount, index);
+                } catch (NumberFormatException exception) {
+                    System.out.println("please input a number in the valid task range. try again");
+                }
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    input = input.substring(input.indexOf(" ") + 1).trim();
+                    int index = Integer.parseInt(input) - 1;
+                    unmark(tasks, taskCount, index);
+                } catch (NumberFormatException exception) {
+                    System.out.println("please input a number in the valid task range. try again");
+                }
             } else {
-                Task t = new Task(input);
-                tasks[taskCount] = t;
-                taskCount++;
-                System.out.println("added: " + t.getDescription());
+                if (taskCount >= MAX_TASKS) {
+                    System.out.println("sorry.. i cannot add anymore tasks!");
+                } else if (input.trim().isEmpty()) {
+                    System.out.println("task description cannot be empty. try again");
+                } else {
+                    Task t = new Task(input);
+                    tasks[taskCount] = t;
+                    taskCount++;
+                    System.out.println("added: " + t.getDescription());
+                }
             }
             input = in.nextLine();
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,28 +1,33 @@
 public class Task {
-    protected String description;
-    protected boolean isDone;
+    private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
+
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "X" : " ");
     }
+
     public String getDescription() {
         return description;
     }
+
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
+
     public void markAsDone() {
         this.isDone = true;
-        System.out.println("hooray! you've completed this task. i've marked it as done: ");
-        System.out.println(this);
     }
+
     public void unmark() {
         this.isDone = false;
-        System.out.println("oh dear, it seems you have NOT completed this task. i've unmarked it: ");
-        System.out.println(this);
+    }
+
+    public boolean isDone() {
+        return isDone;
     }
 }
